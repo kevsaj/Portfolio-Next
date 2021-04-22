ac1c094508b8e97dcba4c4fc4a42f18393ca28e8
Kevin Sajan <kevsaj@gmail.com>
2021-04-21 22:12:52 -0400

    Header component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,24 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.scss'
 
+import Header from '../components/Header'
+// import dynamic from 'next/dynamic'
+
+// const Header = dynamic(
+//   () => import('../components/Header'),
+//   { ssr: false }
+// )
+
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className={`${styles.container} bg-pink-200 flex flex-col`}>
       <Head>
         <title>Kevin Sajan</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      
+      <div>
+        <Header />
+      </div>
     </div>
   )
 }
